4ac74b585875cb446e2ad2de68dde25e83763ee2
Gautam Jethwani <gjethwani1@gmail.com>
2018-07-08 03:44:02 +0800

    Need to fix config

---
+++ spec/trello-updater-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TrelloUpdaterView from '../lib/trello-updater-view';
+
+describe('TrelloUpdaterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
